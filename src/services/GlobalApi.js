a78979c812555ddabaaeb7bf20691f705403eec4
manishkumarth <manish12099@gmail.com>
2025-11-10 12:50:37 +0530

    api modified

--- src/services/GlobalApi.js
+++ src/services/GlobalApi.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
-                      
-const movieBaseUrl = "https://api.themoviedb.org/"
-const api_key = '4c83cb358e54788482e08895d60ca55b'
-const movieGenre = 'https://api.themoviedb.org/3/discover/movie?api_key=your-api-key'
 
-const getTrending = axios.get(movieBaseUrl+"/trending/all/day?api_key="+api_key)
-const getMovieGenre=(id)=>axios.get(movieGenre+"&with_genres="+id)
-export default  {
-    getTrending , getMovieGenre
-}
+import axios from 'axios';
+
+const movieBaseUrl = "https://api.themoviedb.org/3";
+const api_key = "4c83cb358e54788482e08895d60ca55b"; // apna actual TMDB API key daalo
+
+// Trending movies
+const getTrending = axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
+
+// Movies by genre
+const getMovieGenre = (id) => axios.get(`${movieBaseUrl}/discover/movie?api_key=${api_key}&with_genres=${id}`);
+
+export default {
+  getTrending,
+  getMovieGenre
+};
+
+
